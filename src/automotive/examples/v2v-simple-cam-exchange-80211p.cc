653fec45b7976f113757f1d269c146e13cb3c909
Francesco Raviglione <francescorav.es483@gmail.com>
2024-04-22 13:27:59 +0200

    Fix bug in v2v-simple-cam-exchange-80211p
    This commit fixes a bug in v2v-simple-cam-exchange-80211p, that was previously not compiling after the introduction of the VBS and VRU support.
    Additionally, the example has been added again to the src/automotive/examples/CMakeLists.txt file, and it is thus available again.

--- src/automotive/examples/v2v-simple-cam-exchange-80211p.cc
+++ src/automotive/examples/v2v-simple-cam-exchange-80211p.cc
@@ -299,7 +299,8 @@ int main (int argc, char *argv[])
           // Setup the new ETSI Basic Services container
           // The first parameter is true is you want to setup a CA Basic Service (for sending/receiving CAMs)
           // The second parameter should be true if you want to setup a DEN Basic Service (for sending/receiving DENMs)
-          bs_container->setupContainer(true,false);
+          // The third parameter should be true if you want to setup a VRU Basic Service (for sending/receiving VAMs)
+          bs_container->setupContainer(true,false,false);
 
           // Store the container for this vehicle inside a local global BSMap, i.e., a structure (similar to a hash table) which allows you to easily
           // retrieve the right BSContainer given a vehicle ID
