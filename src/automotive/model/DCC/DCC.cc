5800ad7e41ddb00c6944147c378c2e4d0298742b
Diego Gasco <diego.gasco99@gmail.com>
2024-05-29 18:00:53 +0200

    Optimized Reactive DCC

--- src/automotive/model/DCC/DCC.cc
+++ src/automotive/model/DCC/DCC.cc
@@ -52,54 +52,105 @@ void DCC::reactiveDCC()
   std::unordered_map<std::string, std::vector<double>> cbrs = m_metric_supervisor->getCBRValues();
   for (auto it = cbrs.begin(); it != cbrs.end(); ++it)
     {
-      double current_cbr = it->second.back();
-      ReactiveState state;
-      if (current_cbr < m_cbr_thresholds.m_cbr_threshold_relaxed)
-        {
-          state = ReactiveState::Relaxed;
-        }
-      else if (current_cbr < m_cbr_thresholds.m_cbr_threshold_active1)
-        {
-          state = ReactiveState::Active1;
-        }
-      else if (current_cbr < m_cbr_thresholds.m_cbr_threshold_active2)
-        {
-          state = ReactiveState::Active2;
-        }
-      else if (current_cbr < m_cbr_thresholds.m_cbr_threshold_active3)
-        {
-          state = ReactiveState::Active3;
-        }
-      else
-        {
-          state = ReactiveState::Restrictive;
-        }
-
       std::string id = it->first;
       std::string numericPart = id.substr (3);
       long stationId = std::stol (numericPart);
-      ReactiveState oldState;
+      double current_cbr = it->second.back();
+      ReactiveState oldState = ReactiveState::Undefined;
+      ReactiveState state;
       if (m_veh_states.find(stationId) != m_veh_states.end())
         {
           oldState = m_veh_states[stationId];
-          
-          if (oldState == state)
+        }
+
+      if (current_cbr < m_reactive_parameters_relaxed.m_cbr_threshold)
+        {
+          if (oldState == ReactiveState::Undefined)
             {
-              // Nothing to change in the node setup
+              state = ReactiveState::Relaxed;
+            }
+          else if (oldState == ReactiveState::Relaxed)
+            {
               continue;
             }
-
-          if (state - oldState > 1)
+          else
             {
-              // If the new state is much more restrictive than the old one
-              state = static_cast<ReactiveState> (oldState + 1);
-            }
-          else if (oldState - state > 1)
-            {
-              // If the new state is much less restrictive than the old one
               state = static_cast<ReactiveState> (oldState - 1);
             }
         }
+      else if (current_cbr < m_reactive_parameters_active1.m_cbr_threshold)
+        {
+          if (oldState == ReactiveState::Undefined)
+            {
+              state = ReactiveState::Active1;
+            }
+          else if (oldState == ReactiveState::Active1)
+            {
+              continue;
+            }
+          else if (oldState - ReactiveState::Active1 > 0)
+            {
+              state = static_cast<ReactiveState> (oldState - 1);
+            }
+          else
+            {
+              state = static_cast<ReactiveState> (oldState + 1);
+            }
+        }
+      else if (current_cbr < m_reactive_parameters_active2.m_cbr_threshold)
+        {
+          if (oldState == ReactiveState::Undefined)
+            {
+              state = ReactiveState::Active2;
+            }
+          else if (oldState == ReactiveState::Active2)
+            {
+              continue;
+            }
+          else if (oldState - ReactiveState::Active2 > 0)
+            {
+              state = static_cast<ReactiveState> (oldState - 1);
+            }
+          else
+            {
+              state = static_cast<ReactiveState> (oldState + 1);
+            }
+        }
+      else if (current_cbr < m_reactive_parameters_active3.m_cbr_threshold)
+        {
+          if (oldState == ReactiveState::Undefined)
+            {
+              state = ReactiveState::Active3;
+            }
+          else if (oldState == ReactiveState::Active3)
+            {
+              continue;
+            }
+          else if (oldState - ReactiveState::Active3 > 0)
+            {
+              state = static_cast<ReactiveState> (oldState - 1);
+            }
+          else
+            {
+              state = static_cast<ReactiveState> (oldState + 1);
+            }
+        }
+      else
+        {
+          if (oldState == ReactiveState::Undefined)
+            {
+              state = ReactiveState::Restrictive;
+            }
+          else if (oldState == ReactiveState::Restrictive)
+            {
+              continue;
+            }
+          else
+            {
+              state = static_cast<ReactiveState> (oldState + 1);
+            }
+        }
+
       // Get the NetDevice
       Ptr<NetDevice> netDevice = m_traci_client->get_NodeMap()[id].second->GetDevice (0);
 
@@ -186,5 +237,86 @@ void DCC::reactiveDCC()
 
   Simulator::Schedule(m_dcc_interval, &DCC::reactiveDCC, this);
 }
+
+/*
+
+Work in progress
+
+void DCC::ToffUpdateAfterCBRupdate(){
+
+  // Necesito el Toff y el waiting
+  double aux;
+  double waiting;
+
+
+  waiting = Simulator::Now().GetDouble() - (LastTx);
+
+  aux = Tonpp.GetDouble()/m_delta * (Toff.GetDouble() - waiting) / Toff.GetDouble() + waiting;
+
+  if(aux < 25*1000000){
+      aux = 25*1000000;
+    }
+
+  if(aux > 1000000000){
+      Toff = Seconds(1);
+    }
+  else{
+      Toff = NanoSeconds(aux);
+    }
+}
+
+
+void DCC::adaptiveDCC()
+{
+  NS_LOG_INFO ("Starting DCC check");
+
+  NS_ASSERT_MSG (m_bs_map != nullptr, "BS Map not set");
+  NS_ASSERT_MSG (m_traci_client != nullptr, "TraCI client not set");
+  NS_ASSERT_MSG (m_metric_supervisor != nullptr, "Metric Supervisor not set");
+  NS_ASSERT_MSG (m_dcc_interval != Time (Seconds (-1.0)), "DCC interval not set");
+
+  std::mutex &mutex = m_metric_supervisor->getCBRMutex ();
+  mutex.lock ();
+
+  std::unordered_map<std::string, std::vector<double>> cbrs = m_metric_supervisor->getCBRValues ();
+
+  double delta_offset = 0;
+  double aux = 0;
+  double CBR = 0; // To retrieve from each node
+  double CBR_Before = 0; // To retrieve from each node
+
+  m_CBR_its = 0.5*m_CBR_its + 0.25* (CBR_Before + CBR); //Step 1
+
+  aux = m_beta*(m_CBR_target - m_CBR_its);
+
+  if((m_CBR_target - m_CBR_its) > 0){ // step 2
+
+      if(aux > m_Gmax){
+          delta_offset = m_Gmax;
+        }else {
+          delta_offset = aux;
+        }
+
+    }
+  else{
+
+      if(aux > m_Gmin){
+          delta_offset = aux;
+        }else{
+          delta_offset = m_Gmin;
+        }
+    }
+
+  m_delta = (1-m_alpha) * m_delta + delta_offset; //step 3
+
+  if(m_delta > m_delta_max) //step 4
+    m_delta = m_delta_max;
+
+  if(m_delta < m_delta_min) // step 5
+    m_delta = m_delta_min;
+
+  ToffUpdateAfterCBRupdate();
+}
+*/
 }
 
