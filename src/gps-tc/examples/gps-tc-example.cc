23fc1eb3fdbc5ca93756ddd5be9b51207dc8df96
Diego Gasco <diego.gasco99@gmail.com>
2024-12-12 10:25:13 +0100

    Bug fixed in gps-tc-example.cc

--- src/gps-tc/examples/gps-tc-example.cc
+++ src/gps-tc/examples/gps-tc-example.cc
@@ -98,7 +98,7 @@ main (int argc, char *argv[])
     };
 
   /* callback function for node shutdown */
-  SHUTDOWN_FCN shutdownNode = [&] (Ptr<Node> exNode,std::string vehicleID)
+  SHUTDOWN_GPS_FCN shutdownNode = [&] (Ptr<Node> exNode,std::string vehicleID)
     {
       /* stop all applications */
 //      Ptr<appClient> appClient_ = exNode->GetApplication(0)->GetObject<appClient>();
@@ -112,7 +112,7 @@ main (int argc, char *argv[])
       /* NOTE: further actions could be required for a safe shut down! */
     };
 
-  GPS_TC_MAP_ITERATOR(GPSTCMap,GPSTCit) {
+      GPS_TC_MAP_ITERATOR(GPSTCMap,GPSTCit) {
       GPS_TC_IT_OBJECT(GPSTCit)->GPSTraceClientSetup(setupNode,shutdownNode);
       GPS_TC_IT_OBJECT(GPSTCit)->playTrace(Seconds(0));
   }
